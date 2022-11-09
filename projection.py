3f2d6d028606b3f11140265cf83a55812c26395a
Zany-Zachary1 <61282566+Zany-Zachary1@users.noreply.github.com>
2022-11-09 12:38:14 -0500

    Uwa

---
+++ projection.py
@@ -0,0 +1,105 @@
+import pygame
+import numpy as np
+from math import *
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+WIDTH, HEIGHT = 800, 600
+pygame.display.set_caption("Pygame 3D Projection")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+scale = 100
+
+circle_pos = [WIDTH/2, HEIGHT/2]
+
+angle = 0
+
+# Define Vertices
+points = []
+points.append(np.matrix([-1, -1, 1]))
+points.append(np.matrix([1, -1, 1]))
+points.append(np.matrix([1,  1, 1]))
+points.append(np.matrix([-1, 1, 1]))
+points.append(np.matrix([-1, -1, -1]))
+points.append(np.matrix([1, -1, -1]))
+points.append(np.matrix([1, 1, -1]))
+points.append(np.matrix([-1, 1, -1]))
+
+projection_matrix = np.matrix([
+    [1, 0, 0],
+    [0, 1, 0]
+])
+
+projected_points = [
+    [n, n] for n in range(len(points))
+]
+
+def connect_points(i, j, points):
+    pygame.draw.line(screen, BLACK, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+
+# Main Window Loop
+clock = pygame.time.Clock()
+while True:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        # Handle Exiting
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+    # Update Stuff
+    rotation_x = np.matrix([
+        [1, 0, 0],
+        [0, cos(angle), -sin(angle)],
+        [0, sin(angle), cos(angle)],
+    ])
+
+    rotation_y = np.matrix([
+        [cos(angle), 0, sin(angle)],
+        [0, 1, 0],
+        [-sin(angle), 0, cos(angle)],
+    ])
+
+    rotation_z = np.matrix([
+        [cos(angle), -sin(angle), 0],
+        [sin(angle), cos(angle), 0],
+        [0, 0, 1],
+    ])
+
+    angle += 0.01
+
+    screen.fill(WHITE)
+
+    # Drawing Stuff
+    i = 0
+    for point in points:
+        rotated2d = np.dot(rotation_z, point.reshape(3, 1))
+        rotated2d = np.dot(rotation_y, rotated2d)
+        rotated2d = np.dot(rotation_x, rotated2d)
+
+        projected2d = np.dot(projection_matrix, rotated2d)
+
+        # Draw the Vertices
+        x = int(projected2d[0][0] * scale) + circle_pos[0]
+        y = int(projected2d[1][0] * scale) + circle_pos[1]
+
+        projected_points[i] = [x, y]
+        pygame.draw.circle(screen, RED, (x, y), 5)
+
+        i += 1
+
+    # Draw our lines
+    for p in range(4):
+        connect_points(p, (p+1) % 4, projected_points)
+        connect_points(p+4, ((p+1) % 4) + 4, projected_points)
+        connect_points(p, (p+4), projected_points)
+
+    pygame.display.update()
